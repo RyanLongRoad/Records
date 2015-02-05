56e25b334043e5f4b6fbfeafd9655a760035d346
RyanLongRoad <4327@longroad.ac.uk>
2015-02-05 12:18:10 +0000

    update

--- employee pay slip.py
+++ employee pay slip.py
@@ -9,6 +9,7 @@ class employee_details:
         slef.employeeNumber = "-"
         self.hoursWorked = "-"
         self.rateOfPay = "-"
+        self.totalPay = "-"
 
 #main program
 
@@ -16,27 +17,56 @@ new_employee_details = employee_details
         
 name = input("Enter the name of the employee: ")
 number = input("Enter the number of the employee: ")
-hours = input("Enter the hours that the employee worked this week: ")
-pay = input("Enter the hourly rate of pay of the employee: ")
+hours = int(input("Enter the hours that the employee worked this week: "))
+pay = float(input("Enter the hourly rate of pay of the employee: "))
 
 
 employee_details.employeeName = name
 employee_details.employeeNumber = number
 employee_details.hoursWorked = hours
 employee_details.rateOfPay = pay
+employee_details.totalPay = pay*hours
 
 payslip = []
 
 payslip.append(employee_details)
 
-def display():
-    print (" ")
-    print("Pay slip")
+def stars():
 
-    print("Name: {0}".format(employee_details.employeeName))
+    s = "*"
 
-    print("Employee Number: {0}".format(employee_details.employeeNumber))
+    numberOfStars = s * 40
+       
 
-    print("Hours worked: {0}".format(employee_details.hoursWorked))
+    return s, numberOfStars
 
-    print("rate of pay: {0}".format(employee_details.rateOfPay))
+
+
+def display(s, numberOfStars):
+
+    print(" ")
+
+    print (numberOfStars)    
+    
+    print("*Pay slip{0: <33}".format(s))
+    
+    print("*{0: <38}*".format(" "))
+
+    print("*Name: {0: <32}*".format(employee_details.employeeName))
+
+    print("*Employee Number: {0}".format(employee_details.employeeNumber))
+
+    print("*Hours worked: {0}".format(employee_details.hoursWorked))
+
+    print("*Rate of pay: {0}".format(employee_details.rateOfPay))
+    
+    print("*{0: <38}*".format(" "))
+
+    print("*total pay: {0}".format(employee_details.totalPay))
+
+    print(numberOfStars)
+
+##main program
+
+s, numberOfStars = stars()
+display(s, numberOfStars)
